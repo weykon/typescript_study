d3817c43a3ec1565dd37ceb585bcc330ffc2665b
weykon <635883092@qq.com>
2020-09-20 21:17:20 +0800

    update

---
+++ genericity/WeaponSystem.ts
@@ -0,0 +1,22 @@
+namespace 新枪械 {
+    abstract class Weapon {
+
+        abstract comps: Array<WeaponComp>
+        abstract name: string
+
+        public AddComponent<T extends WeaponComp>() {
+
+        }
+    }
+
+    abstract class WeaponComp {
+        abstract name: string
+    }
+
+    class AK47 extends Weapon {
+        comps!: WeaponComp[]
+        name!: string
+    }
+    
+}
+
