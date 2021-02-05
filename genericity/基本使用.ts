ed2c0712b1036e253351c8393f505ec74426ddd0
weykon <635883092@qq.com>
2021-02-05 15:24:15 +0800

    update

---
+++ genericity/基本使用.ts
@@ -0,0 +1,20 @@
+namespace 基本使用 {
+  type Dog<T> = { name: string; type: T };
+  const dog: Dog<number> = { name: "ww", type: 20 };
+
+  class Cat<T> {
+    private type: T;
+    constructor(type: T) {
+      this.type = type;
+    }
+  }
+  const cat: Cat<number> = new Cat<number>(20);
+  function swipe<T, U>(value: [T, U]): [U, T] {
+    return [value[1], value[0]];
+  }
+  swipe<Cat<number>, Dog<number>>([cat, dog]);
+
+  //  泛型推断 infer
+  type Foo<T> = T extends {t: infer Test} ? Test :string 
+
+}
