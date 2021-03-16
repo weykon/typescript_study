b5d1dafc20190a2570d4805493386d7bfae41d3f
weykon <635883092@qq.com>
2021-03-16 09:06:15 +0800

    update

---
+++ 类型系统/type_playground.ts
@@ -0,0 +1,27 @@
+namespace type_playground {
+
+    class A { num!: number }
+    class B { string!: string }
+
+    type a = A;
+
+    type b = B;
+
+    function func<T extends A | B>(t: T) {
+        type C = T extends A ? A : B;
+
+    }
+
+
+    type InferSomething<T> = T extends infer U ? U : any;
+
+
+    type MyFuncReturnValue = ReturnType<() => true>
+
+    type inferred = InferSomething<true>
+
+
+    type InferSomething2<T> = T extends { a: infer A, b: number } ? A : any;
+    type inffered1 = InferSomething2<{a: "hello" , b: 1}>
+
+}
