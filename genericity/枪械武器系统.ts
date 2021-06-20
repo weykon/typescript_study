1d8f3434873fe7baefb61b245b73ea21a7c5117e
weykon <635883092@qq.com>
2021-06-21 03:38:28 +0800

    fix: modify weapon systme

--- genericity/枪械武器系统.ts
+++ genericity/枪械武器系统.ts
@@ -1,70 +1,46 @@
-abstract class Weapon<T>{
-    abstract comps: Array<WeaponComp<T>>
-    abstract name: string
-    public AddComponent<T extends WeaponComp<T>>(t: T) {
-        this.comps.push(t)
-        return t
+namespace WeaponSystem {
+
+    abstract class Weapon<T>{
+        abstract comps: Array<WeaponComp<T>>
+        abstract name: string
+        public AddComponent(t: WeaponComp<T>) {
+            this.comps.push(t)
+            return t
+        }
+        only?: {}
+
     }
 
-}
 
-abstract class WeaponComp<T> {
-    abstract name: string
-    constructor(protected ctor: { new(): T }) { }
-    public WeaponCopm() {
-        return new this.ctor();
+    abstract class WeaponComp<T>{
+        comp_name!: string
+        type!: T
     }
-}
 
-enum WeaponEnum {
-
-}
-
-class AK47 extends Weapon<AK47>{
-    comps: Array<WeaponComp<AK47>> = [];
-    name!: string
-    constructor() {
-        super()
+    abstract class SightOption<T> extends WeaponComp<T>{
+        from!: T
     }
-}
 
-abstract class Model<T extends Weapon> extends WeaponComp {
-    abstract modelData: string
-    abstract modelList: Array<WeaponComp>
-}
-abstract class WeaponPos {
-    constructor(public x: number, public y: number) { }
-}
-
-class WeaponData<T> {
-    private static Config: [
-        { ak47: { name: 'ak47', damge: 100 } },
-        { m4a1: { name: 'm4a1', damge: 80 } }
-    ]
-    public static config<T extends Weapon>(t: T) {
-        let data = {}
-        WeaponData.Config.some(element => {
-            Object.keys(element)[0] === t.name && (data = element)
-            return true
-        });
+    class FourZoom<T extends Weapon<T>> extends SightOption<T>{
+        constructor() {
+            super();
+        }
+        from!: T
+        checkOnly() {
+            this.from.only
+        }
     }
-}
 
-abstract class SightOption<T> extends WeaponComp<SightOption<T>> {
-    abstract name: string
-    constructor(protected ctor: { new(): T }) { super(SightOption.prototype.ctor) }
-    public WeaponCopm() {
-        return new this.ctor();
+
+    class AK47 extends Weapon<AK47>{
+        comps!: WeaponComp<AK47>[]
+        name!: string
+        only!: {}
     }
-}
 
-class ZoomSight extends SightOption {
-    constructor(private ctor: { new(): ZoomSight }, public name: string, public zoom: number) {
-        super(ctor)
-    }
-}
 
-let ak = new AK47()
-console.log(ak)
-ak.AddComponent<WeaponComp<ZoomSight>>()
-console.log(ak)
+    const ak47 = new AK47()
+    const fourzoomcomp = ak47.AddComponent(new FourZoom<AK47>());
+
+
+}
