9b438dba2d22100ac990b9909353a05f2dbcd5a2
weykon <635883092@qq.com>
2022-07-26 10:09:04 +0800

    feat: 协变逆变

---
+++ 协变与逆变/main.ts
@@ -0,0 +1,29 @@
+export namespace Covariance_Inverter {
+    type Cat = { meow: '~' }
+    type Animal = {}
+
+    let cat: Cat = { meow: '~' }
+
+    let animal = {}
+
+
+
+    animal = cat  // 这里不报错
+
+    cat = animal  // 这里报错
+
+
+}
+
+export namespace Covariance_Inverter_from_rust {
+    class Animal { }
+    class Cat extends Animal { }
+    class Dog extends Animal { }
+    function capture<Animal>(x: Cage<Animal>) {
+        x.inner = new Dog();   // 这里报错，但是在填入函数参数不报错，即不该在这里实例化
+    }
+    class Cage<T> {
+        inner: T;
+    }
+    capture(new Cage<Dog>())  // 是这样使用
+}
