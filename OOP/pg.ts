2ed703c26e45e3f59d73d6dc6019b62cc304177f
weykon <635883092@qq.com>
2022-08-23 17:20:10 +0800

    feat: 接口声明函数

--- OOP/pg.ts
+++ OOP/pg.ts
@@ -1,3 +1,10 @@
 export namespace OOP_playground {
-    
+    interface whatWouldBeHappen {
+        (...anys: any[]): number;
+    }
+
+    const a: whatWouldBeHappen = (i: number) => 1
+
+    console.log(a(2));
+
 }
