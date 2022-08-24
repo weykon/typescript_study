16b7285eb4620941ae87b1c172a24f0a639e3efb
weykon <635883092@qq.com>
2022-08-25 00:23:54 +0800

    new function for queue async

--- 异步相关/异步管道.ts
+++ 异步相关/异步管道.ts
@@ -2,20 +2,20 @@ export namespace 异步管道_promise队列 {
 
     let curPromise = Promise.resolve();
 
-    const list = new Array(2)
-    list.fill(0)
-    list.forEach(async (e) => {
-        curPromise = curPromise.then(async () => {
-            await new Promise((suc, fail) => {
-                setTimeout(() => {
-                    console.log(e);
-                    suc(e)
-                }, 1 * 1000);
-            })
-        })
-    })
+    // const list = new Array(2)
+    // list.fill(0)
+    // list.forEach(async (e) => {
+    //     curPromise = curPromise.then(async () => {
+    //         await new Promise((suc, fail) => {
+    //             setTimeout(() => {
+    //                 console.log(e);
+    //                 suc(e)
+    //             }, 1 * 1000);
+    //         })
+    //     })
+    // })
 
-    curPromise.then(() => { console.log('done') })
+    // curPromise.then(() => { console.log('done') })
 
     export function promiseQue<T>(queArr: (() => Promise<T>)[]) {
         let curP = Promise.resolve();
@@ -29,12 +29,50 @@ export namespace 异步管道_promise队列 {
     }
 
     promiseQue<void>([
-        () => new Promise((suc, fail) => {
+        () => new Promise<void>((suc, fail) => {
             console.log('1');
             setTimeout(() => {
                 suc()
             }, 2 * 1000);
         }),
-        () => new Promise((suc, fail) => { console.log('2'); suc(); }),
-    ])
+        () => new Promise<void>((suc, fail) => { console.log('2'); suc(); }),
+    ].map((e: any, i: number) => {
+        return () => new Promise<void>(async (suc, fail) => {
+            console.log(i);
+            setTimeout(() => {
+                suc()
+            }, 1 * 1000);
+        })
+    }
+    ));
+
+
+
+    const resolveInTwoSeconds = () => {
+        return new Promise((resolve) => {
+            setTimeout(() => resolve(2), 2000);
+        })
+    };
+
+    const resolveInThreeSeconds = () => {
+        return new Promise((resolve) => {
+            setTimeout(() => resolve(3), 3000);
+        })
+    };
+
+    const resolveInFiveSeconds = () => {
+        return new Promise((resolve) => {
+            setTimeout(() => resolve(5), 5000);
+        })
+    };
+
+    (async function () {
+        const asyncFunctions = [resolveInTwoSeconds, resolveInThreeSeconds, resolveInFiveSeconds];
+        for await (const asyncFn of asyncFunctions) {
+            const result = await asyncFn();
+            console.log(result);
+        }
+    })();
+
+
 }
